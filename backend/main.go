e7fd62cc7b8b099505a690c0708b6a4f24561c42
ArjunSunny <A00336152@student.tus.ie>
2025-10-27 08:54:43 +0000

    Go backend initial commit

---
+++ backend/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"event-planner/db"
+	"event-planner/routes"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+
+	// ✅ Enable CORS so React frontend can call API
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"},
+		AllowMethods:     []string{"POST", "GET", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	routes.RegisterRoutes(server)
+
+	server.Run(":8080") // localhost:8080
+}
