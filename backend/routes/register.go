6ee18495ec01cfa0318862a22df663018d588d58
ArjunSunnyTus <A00336152@student.tus.ie>
2025-11-12 11:30:06 +0000

    SCRUM-36: event deregistration API integration (#17)

--- backend/routes/register.go
+++ backend/routes/register.go
@@ -33,3 +33,27 @@ func registerForEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Registered for event successfully"})
 }
+
+func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id"})
+		return
+	}
+
+	var event models.Event
+
+	event.ID = eventId
+
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel  event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Cancelled successfully"})
+
+}
